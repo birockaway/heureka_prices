2173987226afdabed3dd94fd5d631b5d5ba3a64c
hrubanj <jiri.hruban@email.cz>
2020-06-08 16:13:36 +0200

    Removed incorrect espace in regex

--- src/main.py
+++ src/main.py
@@ -237,7 +237,7 @@ def process_batch_output(batch_results, material_dictionary, naming_map):
 
     logging.info('Extracting eshop names.')
     output["ESHOP"] = (output["shop_homepage"].map(lambda x: urlparse(x).netloc)
-                       .str.replace(r'(www\\.)', r'', regex=True)
+                       .str.replace(r'(www.)', r'', regex=True)
                        )
     # no need to merge on country as the loop runs only for one country
     logging.info("Merging batch with material map.")
